a5559a077de3f3c275e102d72dcf33111c37bbd4
Luis Calle <luedu1103@gmail.com>
2024-10-20 21:29:07 -0500

    database sql

--- desarrollo/PEP/fuentes/db_profesoft.sql
+++ desarrollo/PEP/fuentes/db_profesoft.sql
@@ -1,9 +1,10 @@
 CREATE TABLE "comments" (
   "id" SERIAL PRIMARY KEY NOT NULL,
   "text" VARCHAR(255) NOT NULL,
-  "grade" DOUBLEPRECISION NOT NULL,
+  "grade" DOUBLE PRECISION NOT NULL,
   "user_id" INTEGER NOT NULL,
-  "profesor_id" INTEGER NOT NULL
+  "profesor_id" INTEGER NOT NULL,
+  "forum_id" INTEGER NOT NULL,
 );
 
 CREATE TABLE "teachers_courses" (
@@ -33,15 +34,14 @@ CREATE TABLE "courses" (
 CREATE TABLE "teachers" (
   "id" SERIAL PRIMARY KEY NOT NULL,
   "name" VARCHAR(255) NOT NULL,
-  "grade" DOUBLEPRECISION NOT NULL
+  "grade" DOUBLE PRECISION NOT NULL
 );
 
 CREATE TABLE "forum" (
   "id" SERIAL PRIMARY KEY NOT NULL,
   "title" VARCHAR(255) NOT NULL,
   "content" varchar(255) NOT NULL,
-  "date" datetime NOT NULL,
-  "comments_id" INTEGER NOT NULL,
+  "date" DATE NOT NULL,
   "user_id" integer NOT NULL
 );
 
@@ -60,6 +60,6 @@ ALTER TABLE "courses" ADD CONSTRAINT "study_plan_id_foreign" FOREIGN KEY ("study
 
 ALTER TABLE "comments" ADD CONSTRAINT "comments_user_id_foreign" FOREIGN KEY ("user_id") REFERENCES "users" ("id");
 
-ALTER TABLE "comments" ADD CONSTRAINT "forum_comments_id_foreign" FOREIGN KEY ("id") REFERENCES "forum" ("comments_id");
+ALTER TABLE "comments" ADD CONSTRAINT "comments_forum_id_foreign" FOREIGN KEY ("forum_id") REFERENCES "forum" ("id");
 
 ALTER TABLE "forum" ADD CONSTRAINT "forum_user_id_foreign" FOREIGN KEY ("user_id") REFERENCES "users" ("id");
