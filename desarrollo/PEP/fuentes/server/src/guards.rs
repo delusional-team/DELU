66003abc8fdff90cf7742a46ef816b38ded1599c
Paolo Flores <pflores.fisi22@gmail.com>
2024-11-05 10:40:42 -0500

    add token verification guard

--- desarrollo/PEP/fuentes/server/src/guards.rs
+++ desarrollo/PEP/fuentes/server/src/guards.rs
@@ -1,7 +1,12 @@
+use std::{collections::BTreeMap, env};
+use jwt::VerifyWithKey;
+use hmac::{Hmac, Mac};
 use rocket::{
     http::Status,
     request::{self, FromRequest},
 };
+use sha2::Sha256;
+use sqlx::{Pool, Postgres};
 
 use crate::api::user_management::AppUser;
 
@@ -17,8 +22,29 @@ impl<'r> FromRequest<'r> for AppUser {
             }
         };
 
-        let token = authorization.split_whitespace().nth(1).unwrap_or_default();
+        let user_token = authorization.split_whitespace().nth(1).unwrap_or(authorization);
+        let secret = env::var("JWT_SECRET").unwrap();
 
-        rocket::request::Outcome::Success(user)
+        let key: Hmac<Sha256> = Hmac::new_from_slice(secret.as_bytes()).unwrap();
+
+        let claims: BTreeMap<String, String> = user_token.verify_with_key(&key).unwrap();
+        let user_email = claims.get("sub").unwrap();
+
+        let app_state = match request.rocket().state::<Pool<Postgres>>() {
+            Some(state) => state,
+            None => return rocket::request::Outcome::Forward(Status::InternalServerError),
+        };
+
+        let user = sqlx::query_as!(
+            AppUser,
+            "SELECT * FROM users WHERE email = $1",
+            user_email,
+        ).fetch_optional(app_state).await.unwrap();
+
+        if user.is_none() {
+            return rocket::request::Outcome::Forward(Status::Unauthorized);
+        }
+
+        rocket::request::Outcome::Success(user.unwrap())
     }
 }
