b6e3698747c991f38444d43e917b0d3cba7a05f5
Luis Calle <luedu1103@gmail.com>
2024-11-06 17:43:32 -0500

    Docker

--- desarrollo/PEP/fuentes/server/src/main.rs
+++ desarrollo/PEP/fuentes/server/src/main.rs
@@ -3,6 +3,10 @@ use dotenvy::dotenv;
 use sqlx::Postgres;
 use sqlx::Pool;
 
+use rocket::config::{Ident, LogLevel};
+use rocket::data::{Limits, ToByteUnit};
+use rocket::Config;
+
 #[macro_use] extern crate rocket;
 
 mod db;
@@ -21,15 +25,27 @@ async fn index(pool: &rocket::State<Pool<Postgres>>) -> Result<&'static str, roc
     }
 }
 
+pub const PORT: u16 = 6969;
+
 #[launch]
 async fn rocket() -> _ {
     dotenv().expect("No .env file");
 
+    let config = Config {
+        port: PORT,
+        address: [0, 0, 0, 0].into(),
+        ident: Ident::none(),
+        log_level: LogLevel::Normal,
+        limits: Limits::new()
+            .limit("forms", 16.mebibytes())
+            .limit("json", 32.mebibytes()),
+        ..Config::default()
+    };
     let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-
     let pool = db::pool::create_pool(&database_url).await.unwrap();
 
     rocket::build()
+        .configure(config)
         .manage(pool)
         .mount(PROFESOFT, routes![index])
         .mount(PROFESOFT, api::user_management::routes())
