8f18c84da2830e65f2f761569432162ba8ab28ef
Paolo Flores <pflores.fisi22@gmail.com>
2024-11-26 02:31:53 -0500

    add trigger to recalculate teachers grade on comments

--- desarrollo/PEP/fuentes/server/migrations/0005_professors_comments.down.sql
+++ desarrollo/PEP/fuentes/server/migrations/0005_professors_comments.down.sql
@@ -1,3 +1,7 @@
 -- Add down migration script here
 
 DROP TABLE IF EXISTS professor_comments;
+
+DROP FUNCTION IF EXISTS recalculate_profesor_grade;
+
+DROP TRIGGER IF EXISTS recalculate_profesor_grade ON professor_comments;
