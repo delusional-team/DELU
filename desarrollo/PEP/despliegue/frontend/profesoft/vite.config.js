bfeb3c2e4221e50bbc9b5a3dffbdf3317c784acd
Luis Calle <luedu1103@gmail.com>
2024-11-27 21:15:25 -0500

    Config

--- desarrollo/PEP/despliegue/frontend/profesoft/vite.config.js
+++ desarrollo/PEP/despliegue/frontend/profesoft/vite.config.js
@@ -1,16 +1,21 @@
 import { defineConfig } from 'vite';
 import dotenv from 'dotenv';
 
-dotenv.config();
+dotenv.config(); // Cargar variables de entorno
 
 export default defineConfig({
-    server: {
-      proxy: {
-        '/profesoft': {
-          target: process.env.VITE_HOST_URL,
-          changeOrigin: true,
-          secure: false,
+  server: {
+    proxy: {
+      '/profesoft': {
+        target: process.env.VITE_HOST_URL, // Asegúrate de que esta variable exista
+        changeOrigin: true,
+        secure: false,
+        configure: (proxy) => {
+          proxy.on('proxyReq', (proxyReq) => {
+            proxyReq.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+          });
         },
       },
     },
-  });
+  },
+});
